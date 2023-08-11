7cf1f9882314687ba3e96ce2fa874f629896c8ea
mohamed <mohamed@me.com>
2023-08-11 23:16:21 +0300

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 /* betty style doc for function main goes there */
 /**
  * main - main function if
- * Description:  Positive anything is better than negative nothing
+ * Description: Positive anything is better than negative nothing
  * Return: 0
  */
 int main(void)
