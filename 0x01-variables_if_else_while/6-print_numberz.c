aa25cd11a3945ac12578275d393b764be72866f9
mohamed <mohamed@me.com>
2023-08-12 03:33:06 +0300

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Description: print numberz
+ * return: 0
+ */
+int main(void)
+{
+int a;
+
+a = 48;
+while (a < 58)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
