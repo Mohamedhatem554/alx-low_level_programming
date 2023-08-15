c513c3ff4696a1fe33ab5d8a8708807ea875e776
mohamed <mohamed@me.com>
2023-08-16 01:40:39 +0300

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,19 +2,20 @@
 
 /**
  * _abs - entry point
+ * @i: number to be checked
  * Return: abs if i is negative
 */
 int _abs(int i)
 {
 
-    
-    
-    if (i < 0)
-    {
-        int abs;
-        
-        abs = i * -1;
-        return(abs);
-    }
-    return (i);
+
+
+	if (i < 0)
+	{
+		int abs;
+
+		abs = i * -1;
+		return(abs);
+	}
+	return (i);
 }
