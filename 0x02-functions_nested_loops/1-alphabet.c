c3c4b94741e6a7afcbb1c0c0ce9fcf2615fec7ef
mohamed <mohamed@me.com>
2023-08-16 00:06:46 +0300

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * Return: 0 always
+*/
+int main(void)
+{
+	
+	int ch;
+
+	void print_alphabet(void)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+}
