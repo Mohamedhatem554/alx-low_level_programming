422b6d58346a4ec861a4b3b594ea410b136bca73
mohamed <mohamed@me.com>
2023-08-16 01:18:36 +0300

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
-
+/**
+ * _isalpha - entry point
+ * @c: character to be checked
+ * Return: 1 if success
+*/
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+	return (1);
 	}
 	return (0);
 }
