fc020c5fdd24c5598df94f8cc0641a11372e52b1
mohamed <mohamed@me.com>
2023-08-16 03:46:43 +0300

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
+/**
+ * print_to_98 - entry point
+ * @n: Number to be counted
+*/
 void print_to_98(int n)
 {
-    
+	int sr;
+
+	for (sr = n; sr <= 98; sr++)
+	{
+		if (sr == 98)
+		{
+			printf("%d", sr);
+			printf("\n");
+		}
+		else
+			printf("%d", sr);
+			printf(",");
+			printf(" ");
+	}
 }
