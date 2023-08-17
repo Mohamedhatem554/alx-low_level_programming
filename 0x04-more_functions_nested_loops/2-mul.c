59abdea91fc7158780793813d93012dcf8c03f54
mohamed <mohamed@me.com>
2023-08-17 19:06:22 +0300

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: first number
+ * @b: second number
+ * Return 0
+*/
+int mul(int a, int b)
+{
+	printf("%d", a * b);
+	return (0);
+}
