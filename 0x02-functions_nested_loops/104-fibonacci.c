13e2293af0e587613c79db77b42e003001e89ab6
mohamed <mohamed@me.com>
2023-08-17 03:17:00 +0300

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,8 @@
 int main(void)
 {
 	int i;
-	unsigned long f1 = 0, f2 = 1, sum, half1, half2, fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long f1 = 0, f2 = 1, sum, half1, half2;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 
 	for (i = 0; i < 92; i++)
 	{
@@ -26,14 +27,16 @@ int main(void)
 		half2 = fib1_half2 + fib2_half2;
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
-			half1 += 1;		half2 %= 10000000000;
+			half1 += 1;
+			half2 %= 10000000000;
 		}
 		printf("%lu%lu", half1, half2);
 		if (i != 98)
 			printf(", ");
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;		fib2_half2 = half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
 	}
 	return (0);
 }
