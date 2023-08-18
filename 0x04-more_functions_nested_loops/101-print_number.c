06419baa8703f943e599aef5114216dcdc61870e
mohamed <mohamed@me.com>
2023-08-19 01:18:29 +0300

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -33,14 +33,14 @@ void print_number(int n)
 		_putchar(i % 10 + '0');
 	}
 	if (i > 999 && i < 9999)
-		{
-			_putchar(i / 1000 + '0');
-			i = i / 100;
-			_putchar(i % 10 + '0');
-			i = n;
-			i = i / 10;
-			_putchar(i % 10 + '0');
-			i = n;
-			_putchar(i % 10 + '0');
-		}
+	{
+		_putchar(i / 1000 + '0');
+		i = i / 100;
+		_putchar(i % 10 + '0');
+		i = n;
+		i = i / 10;
+		_putchar(i % 10 + '0');
+		i = n;
+		_putchar(i % 10 + '0');
+	}
 }
