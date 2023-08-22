04dded5f6c190655a93f411e49a9d0f7515561da
mohamed <mohamed@me.com>
2023-08-22 04:02:39 +0300

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,13 @@ void puts2(char *str)
 
 	i = 0;
 
-	while (*(str + i) < '\0')
+	while (*(str + i) != '\0')
 	{
+		if ((i % 2) == 0)
+		{
 		_putchar(*(str + i));
-		i = i + 2;
+		}
+		i++
 	}
 	_putchar('\n');
 }
