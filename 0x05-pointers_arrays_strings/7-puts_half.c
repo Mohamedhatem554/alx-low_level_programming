2be274377ce4896daa31f85d62138d2d73f08d8f
mohamed <mohamed@me.com>
2023-08-23 01:34:16 +0300

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - entry point
+ * @str: string
+ * Return: Nothing
+*/
+void puts_half(char *str)
+{
+	int c, i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (c = i / 2; str[c] != '\0'; c++)
+	{
+		putchar(str[c]);
+	}
+}
