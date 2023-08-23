0df014bd2fecd2a0bc55fe783bdf0b003ee73042
mohamed <mohamed@me.com>
2023-08-23 03:52:23 +0300

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,4 @@
-#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - entry point
@@ -9,48 +6,6 @@
 */
 int main(void)
 {
-	int N = 10;
-	int i = 0;
-
-	int randomizer = 0;
-
-	srand((unsigned int)(time(NULL)));
-
-
-	char numbers[] = "0123456789";
-
-	char lower[] = "abcdefghijklmnoqprstuvwyzx";
-
-	char UPPER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
-	char symbols[] = "!@#$^&*?";
-
-	char password[N];
-
-	randomizer = rand() % 4;
-
-	for (i = 0; i < N; i++) {
-
-		if (randomizer == 1) {
-			password[i] = numbers[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 2) {
-			password[i] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 3) {
-			password[i] = UPPER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else {
-			password[i] = lower[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-	}
+	printf("hello world");
 	return (0);
 }
