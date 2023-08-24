32f9b8220cdda78dd269e2ed8e1e40dbfc3ca867
mohamed <mohamed@me.com>
2023-08-24 20:32:06 +0300

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - entry point
+ * @s: char
+ * Return: l
+*/
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; *s != '\0'; s++)
+	{
+		l++;
+	}
+	return (l);
+}
+
+/**
  * _strcat - entry point
  * @dest: char
  * @src: source
@@ -18,20 +34,4 @@ char *_strcat(char *dest, char *src)
 	dest[destlen + i] = '\0';
 
 	return (dest);
-}
-
-/**
- * _strlen - entry point
- * @s: char
- * Return: l
-*/
-int _strlen(char *s)
-{
-	int l;
-
-	for (l = 0; *s != '\0'; s++)
-	{
-		l++;
-	}
-	return (l);
 }
