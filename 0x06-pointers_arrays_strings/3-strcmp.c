db7fdc799327c8de557cccdd1ff7e0a3ba3d2fd9
mohamed <mohamed@me.com>
2023-08-25 00:18:38 +0300

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - entry point
+ * @s1: char
+ * @s2: char
+ * Return: (s1 - s2) or (0)
+*/
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 - *s2 != 0)
+	{
+		return (*s1 - *s2);
+	}
+	if (*s1 - *s2 == 0)
+	{
+		return (*s1 - *s2);
+	}
+}
