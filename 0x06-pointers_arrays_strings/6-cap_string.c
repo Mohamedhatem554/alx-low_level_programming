143efe11761638e1053dd6aac2389c669990819f
mohamed <mohamed@me.com>
2023-08-26 02:18:09 +0300

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string - entry point
+ * @str: string
+ * Return: (str) if success
+*/
+char *cap_string(char *str)
+{
+	int n;
+
+	n = 0;
+	while (str[n] != '\0')
+	{
+		if (str[n] == ' '
+		|| str[n] == '	'
+		|| str[n] == '\n'
+		|| str[n] == ','
+		|| str[n] == ';'
+		|| str[n] == '.'
+		|| str[n] == '!'
+		|| str[n] == '?'
+		|| str[n] == '"'
+		|| str[n] == '('
+		|| str[n] == ')'
+		|| str[n] == '{'
+		|| str[n] == '}'
+		)
+		{
+
+			if (str[n + 1] >= 'a' && str[n + 1] <= 'z')
+		{
+			str[n + 1] = str[n + 1] - 32;
+		}
+		}
+
+		n++;
+	}
+	return (str);
+}
