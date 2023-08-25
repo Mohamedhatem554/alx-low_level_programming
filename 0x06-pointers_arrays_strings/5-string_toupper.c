72b3c0ea0ccc3a210a387901c007eaa5d0f08cf4
mohamed <mohamed@me.com>
2023-08-25 03:58:43 +0300

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,47 +1,20 @@
 #include "main.h"
 
+/**
+ * string_toupper - entry point
+ * @str: char
+ * Return: str if success
+*/
 char *string_toupper(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = str[i] - 32;
-        }
-    }
-    return (str);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char str[] = "Look up!\n";
-    char *ptr;
-
-    ptr = string_toupper(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
 }
