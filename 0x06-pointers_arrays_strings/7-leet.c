857279d30cca9d9c767fe303b823d908edd8780e
mohamed <mohamed@me.com>
2023-08-26 03:24:09 +0300

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - entry point
+ * @str: char
+ * Return: str if success
+*/
+char *leet(char *str)
+{
+    char l[] = "aAeEoOtTlL";
+    char num[] = "4433007711";
+    int count, i;
+
+count = 0;
+   while (str[count] != '\0')
+   {
+    i = 0;
+        while (i < 10)
+        {
+            if (str[count] == l[i])
+            {
+                str[count] = num[i];
+            }
+            i++;
+        }
+    count++;
+   }
+   return (str);
+}
