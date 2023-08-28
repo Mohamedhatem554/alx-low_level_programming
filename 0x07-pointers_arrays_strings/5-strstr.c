848838fe9fdbaf07409ac845bb22c8595af00519
mohamed <mohamed@me.com>
2023-08-29 02:36:44 +0300

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strstr - entry point
+ * @haystack: char
+ * @needle: char
+ * Return: NULL
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	if (haystack[i] == *needle)
+	{
+		return (haystack + i);
+	}
+	return ('\0');
+}
