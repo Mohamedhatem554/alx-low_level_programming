1b123cb47b172468a8cca85e738b67530682446c
mohamed <mohamed@me.com>
2023-08-29 03:44:07 +0300

    8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_diagsums(int *a, int size)
+{
+	int i, l1 = 0, l2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		l1 = l1 + a[i * (size + 1)];
+		l2 = l1 + a[(i + 1) * (size - 1)];
+	}
+	printf("%d, ", l1);
+	printf("%d", l2);
+	printf("\n");
+}
