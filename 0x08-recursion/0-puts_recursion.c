a96057df5a18c4b665c4b126b22504e6f854787e
mohamed <mohamed@me.com>
2023-08-30 22:48:04 +0300

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 
 /**
- * _puts_recursion - Write a function that prints a string, followed by a new line.
+ * _puts_recursion - Write a function that prints a string.
  * @s: the string
 */
 void _puts_recursion(char *s)
