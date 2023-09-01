61ab225f7cf513bae6c8c77e56bc5aceceffe128
mohamed <mohamed@me.com>
2023-09-01 23:38:06 +0300

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: string
+ * Return: 0
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	;
+    printf("%d\n", i - 1);
+	return (0);
+}
