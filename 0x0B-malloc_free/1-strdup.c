3b4b1f868e73f61e5c73b702f71a5ddfd94a62e5
mohamed <mohamed@me>
2023-09-06 02:55:05 +0300

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,55 +2,33 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * _strdup - entry point
+ * @str: char
+ * Return: Null
+*/
 char *_strdup(char *str)
 {
-    unsigned int i;
-    char *newstr = NULL;
-    int n;
+	unsigned int i;
+	char *newstr = NULL;
+	int n;
 
-    if (str == NULL)
-    return (NULL);
+	if (str == NULL)
+	return (NULL);
 
-    for (n = 0; str[n] != '\0'; n++)
-    ;
-    newstr = (char *)malloc(n + 1 * sizeof(char));
-    if (newstr != NULL)
-    {
-        for (i = 0; str[i] != '\0'; i++)
-        {
-            newstr[i] = str[i];
-        }
-    }
-    else
-    return (NULL);
-    newstr[i] = '\0';
-    return (newstr);
+	for (n = 0; str[n] != '\0'; n++)
+	;
+	newstr = (char *)malloc(n + 1 * sizeof(char));
+	if (newstr != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			newstr[i] = str[i];
+		}
+	}
+	else
+	return (NULL);
+	newstr[i] = '\0';
+	return (newstr);
 
-}
-
-
-
-
-
-
-
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s;
-
-    s = _strdup("ALX SE");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
 }
