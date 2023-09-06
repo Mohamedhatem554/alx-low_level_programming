9d50c7eeed2a1daa09a4033d25b607ff963ad0fa
mohamed <mohamed@me>
2023-09-06 03:40:29 +0300

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,7 +2,43 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * alloc_grid - entry point
+ * @width: int
+ * @height: int
+ * Return: newarr
+*/
 int **alloc_grid(int width, int height)
 {
-    
+	int **newarr;
+	int i, j;
+
+	if (width <= 0)
+	return (NULL);
+	if (height <= 0)
+	return (NULL);
+
+	newarr = (int **)malloc(sizeof(int *) * height);
+	if (newarr == NULL)
+	return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		newarr[i] = (int **)malloc(sizeof(int) * width);
+		if (newarr[i] == NULL)
+		{
+			for (i -= 1; i >= 0; i--)
+			free(newarr[i]);
+			free(newarr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			newarr[i][j] = 0;
+		}
+	}
+	return(newarr);
 }
