1efa5ed8559b826b9bb8611fdf3fa5f6ab4b051e
mohamed <mohamed@me>
2023-09-12 18:59:35 +0300

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include "dog.h"
 
-
+/**
+ * free_dog - entry point
+ * @d: struct
+*/
 void free_dog(dog_t *d)
 {
-    if (d)
-    {
-        if (d->name)
-        free(d->name);
-        if (d->owner)
-        free(d->owner);
-        if (d == 0)
-        return;
-        
-        free(d);
-    }
+	if (d)
+	{
+		if (d->name)
+		free(d->name);
+		if (d->owner)
+		free(d->owner);
+		if (d == 0)
+		return;
+
+		free(d);
+	}
 }
