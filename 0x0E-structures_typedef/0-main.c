27b3207254f5111b8b3e4d4e071acda4b99d4fe6
mohamed <mohamed@me>
2023-09-12 18:58:20 +0300

    5

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include "dog.h"
 
@@ -12,5 +13,6 @@ int main(void)
 
     my_dog = new_dog("Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    free_dog(my_dog);
     return (0);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,32 +3,17 @@
 #include "dog.h"
 
 
-
-#include <stdio.h>
-#include "dog.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    dog_t *my_dog;
-
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    free_dog(my_dog);
-    return (0);
-}
-
-
 void free_dog(dog_t *d)
 {
     if (d)
     {
+        if (d->name)
         free(d->name);
+        if (d->owner)
         free(d->owner);
+        if (d == 0)
+        return;
+        
         free(d);
     }
 }
