0c37de0d6dbdc8dfa189a8f0d2495b7fe4ba82fc
mohamed <mohamed@me>
2023-09-14 09:30:30 +0300

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - entry point
+ * @size: int
+ * @array: pointer to int
+ * @cmp: pointer to function
+ * Return: 1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size > 0)
+	{
+		if (array != NULL || cmp != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
+				return (i);
+			}
+		}
+	}
+
+	return (-1);
+}
