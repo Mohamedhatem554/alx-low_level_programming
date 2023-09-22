04876eb3f71c5d3b8d534b12379a5e7f0ec938c0
mohamed <mohamed@me>
2023-09-22 03:34:00 +0300

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: header
+ * Return: number of nodes
+*/
+size_t list_len(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
+
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = list_len(head);
+    printf("-> %lu elements\n", n);
+    free(new->str);
+    free(new);
+    return (0);
+}
