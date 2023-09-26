87a249847823e15a5e8cc1d723000dae48bd27d8
mohamed <mohamed@me>
2023-09-27 02:24:12 +0300

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,9 +13,9 @@ void free_listint2(listint_t **head)
 	{
 		while (*head)
 		{
-			ptr = (*head)->next;
-			free(head);
-			*head = ptr;
+			ptr = *head;
+			*head = ptr->next;
+			free(ptr);
 		}
 	}
 }
