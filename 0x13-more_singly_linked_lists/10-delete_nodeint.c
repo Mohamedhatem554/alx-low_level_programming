71226e9818c71d3e69d874bb1c09ee791dbbabab
mohamed <mohamed@me>
2023-09-27 04:38:06 +0300

    10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node
+ * @head: header
+ * @index: position
+ * Return: 1 if success
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *ptr = *head;
+	listint_t *ptr2;
+
+	if (ptr && index == 0)
+	{
+		*head = ptr->next;
+		free(ptr);
+		return (1);
+	}
+	while (ptr)
+	{
+		if (index != 1)
+		{
+			ptr2 = ptr->next;
+			if (ptr2)
+			{
+				ptr->next = ptr2->next;
+				free(ptr2);
+				return (1);
+			}
+		}
+		ptr = ptr->next;
+		index--;
+	}
+	return (-1);
+}
