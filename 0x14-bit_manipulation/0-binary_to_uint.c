ee75abee567358d6e2e17e3544355c621d1262e7
mohamed <mohamed@me>
2023-09-29 04:43:12 +0300

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - binary to unit
+ * @b: string
+ * Return: decimal number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec, n;
+
+	for (dec = 0, n = 0; b[n] != '\0'; i++)
+	{
+		if (b[n] == '1')
+		{
+			dec = (dec << 1) | 1;
+		}
+		else if (b[n] == '0')
+		{
+			dec <<= 1;
+		}
+		else
+		return (0);
+	}
+	return (dec);
+}
