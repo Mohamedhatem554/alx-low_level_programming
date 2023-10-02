9df8bbbfc76cc1892eda6747091314774e2e427d
mohamed <mohamed@me>
2023-10-02 22:03:20 +0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 
 	if (text_content == NULL)
-	text_content = " ";
+	text_content = "";
 
 	for (i = 0; text_content[i] != '\0'; i++)
 	;
