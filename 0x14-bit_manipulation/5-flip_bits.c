08bacdfafeb5054d6ff59911b4112ca7608129bf
mohamed <mohamed@me>
2023-10-26 22:02:59 +0300

    5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - flib bits
+ * @n: number
+ * @m: second number
+ * Return: count
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int or;
+	unsigned int c = 0;
+
+	or = n ^ m;
+	while (or != 0)
+	{
+		if ((or & 1) == 1)
+		c++;
+		or >>= 1;
+	}
+	return (c);
+}
