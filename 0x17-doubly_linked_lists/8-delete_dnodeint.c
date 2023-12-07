8a890f6fb792433c8fbc689c9b06afd2c8a9c000
mohamed <mohamed@me.com>
2023-12-07 23:53:29 +0200

    100

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete node
+ * @head: header
+ * @index: index
+ * Return: n
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	int n = 0;
+
+	return (n);
+}
