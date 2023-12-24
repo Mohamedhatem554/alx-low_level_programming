3a20e45c74b76e4955d95bbe6e55c5b03a0f3a1f
mohamed <mohamed@me.com>
2023-12-24 03:01:47 +0200

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -29,30 +29,3 @@ hash_table_t *hash_table_create(unsigned long int size)
 	return (hash_t);
 
 }
-
-
-
-
-
-
-
-
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "hash_tables.h"
-
-/**
- * main - check the code for
- *
- * Return: Always EXIT_SUCCESS.
- */
-int main(void)
-{
-    hash_table_t *ht;
-
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
-}
