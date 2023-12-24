99702a73511459c43cf6e161be39733038780712
mohamed <mohamed@me.com>
2023-12-24 03:43:00 +0200

    6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,8 +1,30 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_delete - function
+ * @ht: hash table
+ * Return: nothing
+*/
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *pointer = NULL, *head = NULL;
+	unsigned int i = 0;
 
-    
+	while (i < ht->size)
+	{
+		pointer = ht->array[i];
+		while (pointer)
+		{
+		head = pointer->next;
+		free(pointer->value);
+		free(pointer->key);
+		free(pointer);
+		pointer = head;
+		}
+
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+
 }
